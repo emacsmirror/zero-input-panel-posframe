714102090ba87b75a06b87792df696f6f48c2ea8
Yuanle Song <sylecn@gmail.com>
2024-05-27 00:04:26 +0800

    package summary line style fix

--- zero-input-panel-posframe.el
+++ zero-input-panel-posframe.el
@@ -1,4 +1,4 @@
-;;; zero-input-panel-posframe.el --- posframe based zero-input panel implementation. -*- lexical-binding: t -*-
+;;; zero-input-panel-posframe.el --- Posframe based zero-input panel implementation -*- lexical-binding: t -*-
 
 ;; Licensed under the Apache License, Version 2.0 (the "License");
 ;; you may not use this file except in compliance with the License.
